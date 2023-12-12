db04e41f67dd66dec2c5d34bfadd79b59ba99d5c
anastasiamasalova2020 <masnas2004@gmail.com>
2023-12-12 12:49:06 -0500

    added a background with particles for two pages

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,24 +1,21 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import { DocumentTextIcon } from "@heroicons/react/solid";
-
+import particlesConfig from "../particles-config.json";
 
 const Resume = () => {
-    return (
-      <div className="center-content"> {/* Apply the center-content class */}
-      <div className="text-center mb-4"> {/* Center the icon and text */}
-        <DocumentTextIcon className="mx-auto inline-block w-10" />
-        <h1 className="sm:text-4xl text-3xl font-medium title-font text-black">My Resume</h1>
-      </div>
-      <div className="center-iframe">
-        <iframe src="https://drive.google.com/file/d/1mKZscvkcaYH8RQYP4ZGTVAAzQT3AXUzV/preview" width="600" height="720px" allow="autoplay"></iframe>
-      </div>
-      <div style={styles.madeWithLove}>
-        Made with <span role="img" aria-label="heart">❤️</span> by <span style={styles.boldText}>Anastasiya </span> | 2023
-      </div>
-    </div>
-    );
-  };
+  useEffect(() => {
+    window.particlesJS("particles-js", {
+
+      ...particlesConfig,
+      particles: {
+
+        ...particlesConfig.particles,
+        size: {
+          value: 5,
+        },
+      },
+    });
+  }, []);
 
   const styles = {
     madeWithLove: {
@@ -26,10 +23,38 @@ const Resume = () => {
       fontSize: "1rem",
       color: "gray",
       fontStyle: "italic",
-      textAlign: "center"
+      textAlign: "center",
     },
     boldText: {
-      fontWeight: "bold", // Make the text bold
-    }
+      fontWeight: "bold",
+    },
   };
+
+  return (
+    <div className="center-content">
+      <div id="particles-js"></div>
+      <div className="content-wrapper relative z-1">
+        <div className="text-center mb-4">
+          <DocumentTextIcon className="mx-auto inline-block w-10" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-black">
+            My Resume
+          </h1>
+        </div>
+        <div className="center-iframe">
+          <iframe
+            src="https://drive.google.com/file/d/1mKZscvkcaYH8RQYP4ZGTVAAzQT3AXUzV/preview"
+            width="600"
+            height="720px"
+            allow="autoplay"
+          ></iframe>
+        </div>
+        <div style={styles.madeWithLove}>
+          Made with <span role="img" aria-label="heart">❤️</span> by{" "}
+          <span style={styles.boldText}>Anastasiya </span> | 2023
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Resume;
